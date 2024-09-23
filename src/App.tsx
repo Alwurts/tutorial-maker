6f8c60154b114e79142f67f89c9b7ba595f21370
Alwurts <jota780@gmail.com>
2024-09-22 19:39:57 -0600

    Add shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 function App() {
-	return <h1 className="text-3xl text-red-500 font-bold underline">Hello world!</h1>;
+	return (
+		<div className="gap-5 flex flex-col items-center justify-center h-screen">
+			<h1 className="text-3xl font-bold underline">Hello world!</h1>
+			<Button variant={"outline"}>Click me</Button>
+		</div>
+	);
 }
 
 export default App;
